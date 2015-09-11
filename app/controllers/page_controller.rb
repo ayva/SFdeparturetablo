1d67f5fd47e469ae9a237356a75d9ee8e8e187e8
ayva <olga.ayva@gmail.com>
2015-09-11 16:51:28 -0700

    departure schedule for nearby stops by ip location

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,49 @@
+class PageController < ApplicationController
+
+  def index
+    @client_ip = find_ip
+    @locate=city_state_for_index_info
+
+    @departures=Tablo.new(@client_ip).departures
+  
+  end
+
+
+  private
+
+  def locate_by_ip
+    request.location
+  end
+
+  def address_from_ip
+    if locate_by_ip && locate_by_ip.address != "Reserved"
+        return locate_by_ip.address
+    end
+  end
+
+  def city_state_for_index_info
+    if address_from_ip
+      city = locate_by_ip.city
+      state = locate_by_ip.state
+      return "#{city},#{state}"
+    else
+      return "San-Francisco, CA"
+    end
+  end
+  
+  def find_ip
+    if request.remote_ip == '127.0.0.1' || request.remote_ip=='::1'
+      # Hard coded remote address
+      return "208.113.83.165"
+      #NY '123.45.67.89'
+    else
+      return request.remote_ip
+    end
+  end
+
+  def address_from_ip
+    if locate_by_ip && locate_by_ip.address != "Reserved"
+        return locate_by_ip.address
+    end
+  end
+end
---
+++ app/models/tablo.rb
@@ -0,0 +1,125 @@
+class Tablo
+
+  attr_reader :data_json, :data, :agency, :stops,:departures
+
+  def initialize(ip)
+    @base_url="http://webservices.nextbus.com/service/publicXMLFeed?"
+    setCoordinates(ip);
+
+    @radius=0.004
+
+    #Stores all trasport and stops with location for Northern California
+    @agency = {}
+    buildAgencies();
+    buildRoutes();
+    buildStops();
+
+    #Looking for stops within geo radious
+    nearbyStops();
+    
+    #Checking for departures for stops above
+    getDepartures(@stops);
+    
+  end
+
+
+  def setCoordinates(ip)
+    location = Geokit::Geocoders::IpGeocoder.geocode(ip)
+    if !location.longitude.nil? && !location.latitude.nil?
+      @lon=location.longitude
+      @lat=location.latitude
+    else
+      @lon=-122.3117
+      @lat=37.9158
+    end  
+  end
+
+  def getData(str)
+    return JSON.parse(HTTParty.get(@base_url+str).to_json)
+  end
+
+  def buildAgencies
+    all = getData("command=agencyList")
+    all["body"]["agency"].each do |agency|
+      if agency["regionTitle"]=="California-Northern" 
+        #&& agency["tag"]=="emery"
+        @agency[agency["tag"]]=agency["title"]
+        #for testing will take only first agent - emery
+        break;
+      end
+    end
+  end
+
+  def buildRoutes
+    @agency.keys.each do |key|
+      @agency[key]=getData("command=routeList&a="+key)["body"]["route"]
+    end
+  end
+
+  def buildStops
+    @agency.keys.each do |agent|
+      @agency[agent].each do |routeTag|
+        routeTag["route"]= getData("command=routeConfig&a="+agent+"&r="+routeTag["tag"])["body"]["route"]
+      end
+    end
+  end
+
+
+
+  def nearbyStops
+    @stops={}
+    @agency.keys.each do |agent|
+      @stops[agent]={}
+      @agency[agent].each do |route|
+        @stops[agent][route["route"]["tag"]]={}
+        latMax=route["route"]["latMax"].to_f
+        latMin=route["route"]["latMin"].to_f
+        lonMax=route["route"]["lonMax"].to_f
+        lonMin=route["route"]["lonMin"].to_f
+        
+        #Check if location within whole route coordinates
+        if latMax>@lat && latMin<@lat && lonMax>@lon && lonMin<@lon
+          
+
+          #If coord within route, looking for stops within radius
+          route["route"]["stop"].each do |stop|
+            
+            if( (stop["lat"].to_f-@radius) < @lat) && (@lat < (stop["lat"].to_f+@radius) )&& ((stop["lon"].to_f-@radius) < @lon) && (@lon < (stop["lon"].to_f+@radius))
+              puts agent
+              print route["route"]["tag"], stop["tag"]
+
+              @stops[agent][route["route"]["tag"]][stop["tag"]] = true
+            
+            end
+          
+          end
+        end
+
+      end
+    end
+      
+  end
+
+  def getDepartures(stops)
+   
+    @stops.keys.each do |agency|
+      #We have to get departure for each agency's stops
+      str="command=predictionsForMultiStops&"+"a=#{agency}"
+      
+      @stops[agency].keys.each do |route_tag|
+        @stops[agency][route_tag].keys.each do |stopTag|
+          str+="&stops=#{route_tag}|#{stopTag}"
+        end
+      end
+      @departures=getData(str)
+      #puts str
+    end
+    
+
+  end
+
+
+  
+
+
+end
